f0577a8c42d11273f9021f497252926ad8b77604
CarlosGMI <carlosmaldonado@correo.unimet.edu.ve>
2023-03-22 21:08:59 -0400

    Start project. Add basic dependencies

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
+  title: '57Blocks Test',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+})
+</script>
